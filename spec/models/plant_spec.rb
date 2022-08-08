00256f34ccf7e97eb3bbf5dc4604979f935d4033
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-08-08 08:24:20 -0700

    tested relationships

--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Plant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :description}
+    it {should validate_presence_of :days_to_harvest}
+  end
+
+  describe "relationships" do
+    it { should have_many(:plant_plots) }
+    it { should have_many(:plots) }
+  end
 end
